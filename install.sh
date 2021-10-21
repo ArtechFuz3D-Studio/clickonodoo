0f5d26cd239a7bd0d1a1d9b8702f13b82c9aaae7
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-10-21 04:43:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,15 @@ sudo apt-get install curl -y
 sudo apt-get install wget -y
 sudo apt-get update
 
+# Install Node via Node Version Manager
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# run these commands to startup nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# Install Node
+nvm install node
+
 # Install Docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
